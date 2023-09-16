9fa3fe9c5e4a2b4c6b044a0298da9ec5a7a1b916
Armandase <adamiens@student.42angouleme.fr>
2023-09-16 19:25:43 +0200

    need to make a function which return open list of current node and a function to compute h

--- inc/algorithm.hpp
+++ inc/algorithm.hpp
@@ -5,4 +5,31 @@
 
 void    aStarAlgorithm(vector2d&);
 
+typedef struct t_node
+{
+    vector2d    grid;
+    int         g;
+    struct t_node        *parent;
+} node;
+
+
+
+/*
+g est égal au nombre de coup, 
+par rapport a la position initiale,
+pour arrivé a la position actuel
+
+h (version manhattan) est egal à la somme
+du nombre de coup que chaque tuile doit faire pour arrive à sa position final
+
+h (version hamming) est egal aux nombres de tuiles mal placées
+
+h (version linear conflict) est egal à la version manhattan multiplié par 2 *
+le nombre de conflit (si deux tuiles doivent échanger de place en ligne ou colonne)
+
+f = g + h
+*/
+
+
+
 #endif
