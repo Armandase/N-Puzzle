9bb5f267dc47b0796c807380bc92dd9351995c9f
Nathan Locussol <nlocusso@1A6.42angouleme.fr>
2023-09-17 16:35:56 +0200

    change expend with node struct

--- src/main.cpp
+++ src/main.cpp
@@ -71,15 +71,15 @@ int main (int ac, char **av) {
             puzzle = generate_puzzle();
         }
         // EXPEND NODE FUNCTION THAT RETURN VECTOR<VECTOR2D>
-        print_puzzle(puzzle);
-        std::cout << "EXPEND NODES:\n" << std::endl;
-        node puzzle_node;
-        puzzle_node.grid = puzzle;
-        puzzle_node.g = 0;
-        puzzle_node.parent = NULL;
-        std::vector<node> expend_list = expend_node(puzzle_node);
-        for (int i = 0; i < expend_list.size(); i++)
-            print_puzzle(expend_list[i].grid);
+        // print_puzzle(puzzle);
+        // std::cout << "EXPEND NODES:\n" << std::endl;
+        // node puzzle_node;
+        // puzzle_node.grid = puzzle;
+        // puzzle_node.g = 0;
+        // puzzle_node.parent = NULL;
+        // std::vector<node> expend_list = expend_node(puzzle_node);
+        // for (int i = 0; i < expend_list.size(); i++)
+        //     print_puzzle(expend_list[i].grid);
 
         // MANHATTAN HEURISTIC FUNCTION
         // vector2d final = finalPuzzle(puzzle.size());
